acdd4c6b3f70acbb162964b5cf441ecf7acf3b39
teppanomk <teppanom@siit.tu.ac.th>
2026-03-13 13:44:54 +0700

    Update script.js

--- script.js
+++ script.js
@@ -172,4 +172,17 @@ sendMessage();
 
 }
 
+
+// Dark mode toggle
+
+const toggleBtn = document.getElementById("darkToggle");
+
+toggleBtn.addEventListener("click", function(){
+ document.body.classList.toggle("dark-mode");
+
+ if(document.body.classList.contains("dark-mode")){
+  toggleBtn.innerText = "☀️ Light Mode";
+ } else{
+  toggleBtn.innerText = "🌙 Dark Mode";
+ }
 });
